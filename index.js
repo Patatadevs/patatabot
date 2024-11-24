cc031f1ebb06f99f84652a77fc229c7f17c32d31
GoblinKing <68219518+goblinkingdev@users.noreply.github.com>
2024-11-24 19:05:33 +0100

    Update index.js

--- index.js
+++ index.js
@@ -15,6 +15,14 @@ const client = new Client({
   ],
 });
 
+client.commands = new Collection()
+const commandFiles = fs.readdirSync("./commands").filter((file) => file.endsWith(".js"))
+
+for (const file of commandFiles) {
+  const command = await import(`./commands/${file}`);
+  client.commands.set(command.default.data.name, command.default);
+}
+
 const userMessageCounts = new Map(); // Rastrea mensajes por usuario
 
 client.on('ready', () => {
